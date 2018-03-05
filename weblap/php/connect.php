39408352888e8e79ca8590be0f5bfd3c5d38e6bb
HegyiBazsi <h.bazsi940802@gmail.com>
2018-03-05 09:20:42 +0100

    v2.0
    -A NEW ERA
    -az sql kapcsolat működik és mostmár magyar ékezeteket is fel tudunk vinni
    -LETS ALL CELEBRATE!!!!!

--- weblap/php/connect.php
+++ weblap/php/connect.php
@@ -11,9 +11,9 @@
     $mysqllink=mysqli_connect($host,$user,$pass) or die("Could not connect");
 
     // adatbazis kivalasztasa
-    mysql_query("SET NAMES UTF8", $mysqllink);
-    mysql_query("SET CHARACTER SET UTF8", $mysqllink);
     mysqli_select_db($mysqllink,$db) or die("Could not select database");
+    mysqli_query($mysqllink,"SET NAMES UTF8");
+    mysqli_query($mysqllink,"SET CHARACTER SET UTF8");
 
 
 ?>
--- weblap/post.php
+++ weblap/post.php
@@ -1,34 +1,34 @@
+<?php
+      header("Content-type: text/html; charset=utf-8");
+      if(isset($_POST['lastname']))
+      {
+        include "php/connect.php";
+        $fname = $_POST['firstname'];
+        $lname = $_POST['lastname'];
+        $bdate = $_POST['birthdate'];
+        $zip =  $_POST['cityzip'];
+        $city = $_POST['city'];
+        $street = $_POST['street'];
+        $address = $_POST['address'];
+        $tnum = $_POST['phonenumber'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $sql = "INSERT INTO `Customers`(`FirstName`, `LastName`, `BirthDate`, `CityZip`, `City`, `Street`, `Address`, `Telnum`, `Email`, `Password`) VALUES ('$fname','$lname','$bdate','$zip','$city','$street','$address','$tnum','$email','$password');";
+        $resultset = mysqli_query($mysqllink, $sql ) or die("data transfer error: ".mysqli_error($mysqllink));
+        mysqli_close($mysqllink);
 
-  <?php
-        if(isset($_POST['lastname']))
-        {
-          include "php/connect.php";
-          $fname = $_POST['firstname'];
-          $lname = $_POST['lastname'];
-          $bdate = $_POST['birthdate'];
-          $zip =  $_POST['cityzip'];
-          $city = $_POST['city'];
-          $street = $_POST['street'];
-          $address = $_POST['address'];
-          $tnum = $_POST['phonenumber'];
-          $email = $_POST['email'];
-          $password = $_POST['password'];
-          $sql = "INSERT INTO `Customers`(`FirstName`, `LastName`, `BirthDate`, `CityZip`, `City`, `Street`, `Address`, `Telnum`, `Email`, `Password`) VALUES ('$fname','$lname','$bdate','$zip','$city','$street','$address','$tnum','$email','$password');";
-          $resultset = mysqli_query($mysqllink, $sql ) or die("data transfer error: ".mysqli_error($mysqllink));
-          mysqli_close($mysqllink);
+        unset($_POST['']);
+        unset($_POST['firstname']);
+        unset($_POST['lastname']);
+        unset($_POST['birthdate']);
+        unset( $_POST['cityzip']);
+        unset( $_POST['city']);
+        unset($_POST['street']);
+        unset( $_POST['address']);
+        unset($_POST['phonenumber']);
+        unset($_POST['email']);
+        unset( $_POST['password']);
 
-          unset($_POST['']);
-          unset($_POST['firstname']);
-          unset($_POST['lastname']);
-          unset($_POST['birthdate']);
-          unset( $_POST['cityzip']);
-          unset( $_POST['city']);
-          unset($_POST['street']);
-          unset( $_POST['address']);
-          unset($_POST['phonenumber']);
-          unset($_POST['email']);
-          unset( $_POST['password']);
-
-          header('Location: index.html');
-        }
-  ?>
+        header('Location: index.html');
+      }
+?>
