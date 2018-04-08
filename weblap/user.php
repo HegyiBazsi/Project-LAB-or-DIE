f7817547b271756d98ac281b4eaa3de0025d6654
csibe93 <35483722+csibe93@users.noreply.github.com>
2018-04-08 09:18:49 +0200

    v2.1.1
    User.php reszponzív

--- weblap/user.php
+++ weblap/user.php
@@ -14,16 +14,15 @@
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
 
       <link rel="icon" href="./img/admin/admin2.png">
-
+	  <link href="css/user.css" type="text/css" rel="stylesheet" media="screen,projection"/>
     <!--Let browser know website is optimized for mobile-->
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
       <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
     </head>
 
-
     <body>
-
+	<main>
 	<nav class="navbar-fixed-top">
        	<div class="nav-wrapper">
        		<a href="#!" class="brand-logo center white-text">P-LID</a>
@@ -437,7 +436,7 @@
 
 
       </div><!-- ./container -->
-
+		</main>
       <!--FOOTER-->
         <footer class="page-footer blue">
           <div class="container">
